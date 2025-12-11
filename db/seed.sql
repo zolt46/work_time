138625c0b349c3703836231dbc01aa2c68375d44
zolt46 <seewoopark46@gmail.com>
2025-12-11 20:35:54 +0900

    new commit

--- db/seed.sql
+++ db/seed.sql
@@ -14,11 +14,11 @@ VALUES
 
 INSERT INTO auth_accounts (user_id, login_id, password_hash)
 VALUES
-    ('11111111-1111-1111-1111-111111111111', 'master', '$2b$12$9QZP3p3A9DXwhkH5gGbVfewDMn9CYOMsg/JAyqJ/3aPr2TPpn1Bca'), -- Password: Master123!
-    ('22222222-2222-2222-2222-222222222222', 'operator', '$2b$12$1fTAO1OAPYpNoafHKo57ruIPM4lNi8LJQ73Pe6ck/dED8kyTpcnQW'), -- Password: Operator123!
-    ('33333333-3333-3333-3333-333333333333', 'kim', '$2b$12$EBuqqPK7SoxE/mkOTZcGNOx7/3Enik1MsduxrU9aiwupZRA49PIfy'), -- Password: Member123!
-    ('44444444-4444-4444-4444-444444444444', 'lee', '$2b$12$EBuqqPK7SoxE/mkOTZcGNOx7/3Enik1MsduxrU9aiwupZRA49PIfy'),
-    ('55555555-5555-5555-5555-555555555555', 'park', '$2b$12$EBuqqPK7SoxE/mkOTZcGNOx7/3Enik1MsduxrU9aiwupZRA49PIfy');
+    ('11111111-1111-1111-1111-111111111111', 'master', '$2b$12$pSX/BzwVIcXXHPNdD6LRueFZnHu7aQ4mZPDE5IZ8T.MpYIxaFmjPC'), -- Password: Master123!
+    ('22222222-2222-2222-2222-222222222222', 'operator', '$2b$12$HtMj5ZEZ7YfceJy4tDtW1eAneriBJIg/pGDPIX.SKp9xC7A36QGfK'), -- Password: Operator123!
+    ('33333333-3333-3333-3333-333333333333', 'kim', '$2b$12$bpgqOwoDZy47/tUcY2f8uOaqk2tqLlS9m.uvmBKnl3SXsyZ65wAIK'), -- Password: Member123!
+    ('44444444-4444-4444-4444-444444444444', 'lee', '$2b$12$bpgqOwoDZy47/tUcY2f8uOaqk2tqLlS9m.uvmBKnl3SXsyZ65wAIK'),
+    ('55555555-5555-5555-5555-555555555555', 'park', '$2b$12$bpgqOwoDZy47/tUcY2f8uOaqk2tqLlS9m.uvmBKnl3SXsyZ65wAIK');
 
 -- Shifts (weekday: 0=Mon ... 6=Sun)
 INSERT INTO shifts (id, name, weekday, start_time, end_time, location)
